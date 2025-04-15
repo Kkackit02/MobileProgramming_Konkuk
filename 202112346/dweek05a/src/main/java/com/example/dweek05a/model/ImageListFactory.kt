30c8d6e8947a3e62086660ba9600be88a56c27b8
GeunNyeong Jeong <kkackit@gmail.com>
2025-04-15 21:38:27 +0900

    ADD | Coroutine & Lazy Component

--- 202112346/dweek05a/src/main/java/com/example/dweek05a/model/ImageListFactory.kt
+++ 202112346/dweek05a/src/main/java/com/example/dweek05a/model/ImageListFactory.kt
@@ -25,6 +25,18 @@ object ImageListFactory {
             buttonType = ButtonType.EMOJI,
             likes = 100,
             dislikes = 10
+        ),
+        ImageData(
+            image = ImageUri.ResImage(R.drawable.img1),
+            buttonType = ButtonType.ICON,
+            likes = 120,
+            dislikes = 13
+        ),
+        ImageData(
+            image = ImageUri.ResImage(R.drawable.img3),
+            buttonType = ButtonType.BADGE,
+            likes = 12,
+            dislikes = 53
         )
 
 
